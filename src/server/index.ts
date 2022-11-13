33503f635cf7d765415a1577e90e35099bb22414
rodo08 <rodrigo.rosalesmoya@gmail.com>
2022-11-13 10:55:27 +0100

    Fix start server, app and update no-unused-vars error at server index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,6 @@
 import "../loadEnvironment.js";
-import debugCreator from "debug";
-
-import chalk from "chalk";
 
 import type { Express } from "express";
-
-const debug = debugCreator("users:server");
 
 const startServer = async (app: Express, port: number) => {
   await new Promise((resolve, reject) => {
